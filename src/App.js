6ac5f9ee6f6854dfe2045ce7de74a6e9cb5d70b8
ROSH <imroshni3@gmail.com>
2022-11-17 16:54:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './App.css';
 import Tweet from './Tweet';
 
 function App(){
 
   return(
-    <Tweet/>
+    <div>
+      <h1>Tweet</h1>
+      <div class="contents">
+      <Tweet uname="meow" content="purraaaw"/>
+      <Tweet uname="fart" content="I hate Elon Musk"/>
+      <Tweet uname="litchi" content="I like to drink myself"/>
+    </div>
+    </div>
   );
 }
 
