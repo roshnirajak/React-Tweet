c6ab82dc338b743933907f1a68010f9423b6600c
ROSH <imroshni3@gmail.com>
2022-11-27 13:44:06 +0530

    Create array_api.js

---
+++ src/array_api.js
@@ -0,0 +1,19 @@
+const Menu=[
+    {
+        uname:"@kittin",
+        content:"purrrraaaaaw"
+    },
+    {
+        uname:"@china",
+        content:"This christmas we gift u covid"
+    },
+    {
+        uname:"@zendeya",
+        content:"Ngl Loved spiderman more than Tom"
+    },
+    {
+        uname:"@flick",
+        content:"my rival made my day"
+    }
+];
+export default Menu;
